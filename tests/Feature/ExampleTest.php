257c650ee3c6c093eaa0eae3aaa8488cbec0cf2e
himanshuthedeveloper <himanshu32301@gmail.com>
2021-04-06 17:23:15 +0530

    fixing bug

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,11 +4,14 @@ namespace Tests\Feature;
 
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -17,13 +20,18 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
        
-        $user = User::factory()->create([
-            'name' => 'NewData',
+        $user = User::factory()->create(
+            [
+            'name' => 'Himanshu Singh',
             'email' => 'newEmail@gmail.com'
-            ]);
-        // $response = $this->actingAs($user) ->get('/');
-        // $response->assertStatus(200);
-        $this->assertEquals('newEmail@gmail.com',$user->email);
+            ]
+        );
+    //     $response = $this->actingAs($user) ->get('/');
+    //    $response->assertStatus(200);
+       
+        $this->assertEquals('Himanshu Singh',$user->fullname);
+
+       
     }
  
 }
